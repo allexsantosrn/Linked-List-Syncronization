8af525b8a2f56cbd797e24e56437c8f8daabfc29
allexkid <allexsantosrn@gmail.com>
2022-01-10 21:16:28 -0300

    Update inicial.
    Update inicial do projeto.

---
+++ src/core/Main.java
@@ -0,0 +1,14 @@
+package core;
+
+public class Main {
+
+	// Definição das threads a serem utilizadas.
+	private static final int NUM_THREADS = 4;
+
+	public static void main(String[] args) {
+		
+		System.out.println("teste");
+
+	}
+
+}
