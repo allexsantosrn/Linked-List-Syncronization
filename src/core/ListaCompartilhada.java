beead37623e72b759d899fa587d09ee57b2c0fe5
allexkid <allexsantosrn@gmail.com>
2022-01-12 12:06:09 -0300

    Atualizando lista compartilhada
    Atualizando algumas informações da classe lista compartilhada.

--- src/core/ListaCompartilhada.java
+++ src/core/ListaCompartilhada.java
@@ -1,5 +1,14 @@
 package core;
 
+import java.util.LinkedList;
+
 public class ListaCompartilhada {
+	
+	private LinkedList<Integer> lista;
+	
+	public ListaCompartilhada () {
+		
+		lista = new LinkedList<>();
+	}
 
 }
--- src/core/Main.java
+++ src/core/Main.java
@@ -6,8 +6,39 @@ public class Main {
 	private static final int NUM_THREADS = 4;
 
 	public static void main(String[] args) {
-		
-		System.out.println("teste");
+
+		ListaCompartilhada listacompartilhada = new ListaCompartilhada();
+
+		// Criando os vetores. //
+		BuscarItem[] buscar = new BuscarItem[NUM_THREADS];
+		InserirItem[] inserir = new InserirItem[NUM_THREADS];
+		RemoverItem[] remover = new RemoverItem[NUM_THREADS];
+
+		// Criando as threads. //
+		for (int i = 0; i < NUM_THREADS; i++) {
+			buscar[i] = new BuscarItem("ThreadBusca " + (i + 1), listacompartilhada);
+			inserir[i] = new InserirItem("ThreadInserção " + (i + 1), listacompartilhada);
+			remover[i] = new RemoverItem("ThreadRemoção " + (i + 1), listacompartilhada);
+		}
+
+		// Iniciando as threads. //
+		for (int i = 0; i < NUM_THREADS; i++) {
+			buscar[i].start();
+			inserir[i].start();
+			remover[i].start();
+		}
+
+		// Finalizando as threads. //
+		try {
+			for (int i = 0; i < NUM_THREADS; i++) {
+				buscar[i].join();
+				inserir[i].join();
+				remover[i].join();
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
